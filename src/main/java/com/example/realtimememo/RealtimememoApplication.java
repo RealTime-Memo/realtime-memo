86b0303caeb34640cdbbc18f23e830d5355481c5
hankug <hankug1@naver.com>
2022-04-21 15:46:22 +0900

    init

---
+++ src/main/java/com/example/realtimememo/RealtimememoApplication.java
@@ -0,0 +1,13 @@
+package com.example.realtimememo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimememoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealtimememoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/realtimememo/RealtimememoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.realtimememo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimememoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
